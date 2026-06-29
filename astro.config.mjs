a3b165c913153e46f7acdf381d5103840e2c65a7
maliffadlan <maliffadlan@gmail.com>
2026-06-30 00:44:23 +0700

    feat: add @astrojs/sitemap for SEO auto-generated sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://mectovlinux.mectov.my.id',
+  integrations: [sitemap()],
+});
